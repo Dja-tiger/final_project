40d6a4af6ec761a946db83fe4b2bb73d42b3d9e8
Dja_tiger <jalilli.jalil@gmail.com>
2023-04-01 03:40:39 +0300

    Подключение через airflow docker-compose к БД, находящаяся локально. YAML(extra_hosts: - "host.docker.internal: 5433")

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-host = "localhost"
+# host = "localhost"
 user = "dja_tiger"
 password = ""
 db_name = "final_work_bd"
 port = 5433
-# host = 'host.docker.internal'
+host = 'host.docker.internal'
+# host = "docker.for.mac.host.internal"
--- modules/config.py
+++ modules/config.py
@@ -1,7 +1,7 @@
-host = "localhost"
+# host = "localhost"
 user = "dja_tiger"
 password = ""
 db_name = "final_work_bd"
 port = 5433
 # host = "docker.for.mac.host.internal"
-# host = 'host.docker.internal'
+host = 'host.docker.internal'
